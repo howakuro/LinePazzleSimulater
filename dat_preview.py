985b9369f9b2bedcd090ddec1ca223e8b858a922
howakuro <tkok2568@gmail.com>
2020-01-29 15:29:49 +0900

    add dat_preview.py dat_preview.pyファイルの追加

---
+++ dat_preview.py
@@ -0,0 +1,20 @@
+import pickle
+import os
+import numpy as np
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+path = os.path.join(os.getcwd(), "game_record.dat")
+if os.path.isfile(path): #ファイルが存在するときファイル読み込み
+	with open(path, 'rb') as f:
+		data = pickle.load(f)
+
+data = np.array(data)
+m_score, m_tile = np.max(data, axis=0)
+a_score, a_tile = np.average(data, axis=0)
+print("Max Score     :", m_score)
+print("Max Tile      :", m_tile)
+print("Average Score :", a_score)
+print("Average Tile  :", a_tile)
+print("Play num      :", data.size//2)
+for i in data:
+	print(i)
